9a8912482a326d1032b28dbd204b029051f2691f
@Jhenriquee <jriquee@gmail.com>
2020-07-06 21:16:37 -0300

    Project scaffold with spring initializer.

---
+++ src/main/kotlin/com/jh/controleponto/ControlepontoApplication.kt
@@ -0,0 +1,11 @@
+package com.jh.controleponto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControlepontoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ControlepontoApplication>(*args)
+}
---
+++ src/test/kotlin/com/jh/controleponto/ControlepontoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jh.controleponto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ControlepontoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
