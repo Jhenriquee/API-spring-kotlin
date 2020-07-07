66a95aa48de21b413b3707b71b2f219915629682
@Jhenriquee <jriquee@gmail.com>
2020-07-06 22:08:50 -0300

    Creating repositories

---
+++ src/main/kotlin/com/jh/controleponto/repositories/EmpresaRepository.kt
@@ -0,0 +1,9 @@
+package com.jh.controleponto.repositories
+
+import com.jh.controleponto.documents.Empresa
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface EmpresaRepository : MongoRepository<Empresa, String> {
+
+    fun findByCnpj(cnpj: String): Empresa
+}
---
+++ src/main/kotlin/com/jh/controleponto/repositories/FuncionarioRepository.kt
@@ -0,0 +1,11 @@
+package com.jh.controleponto.repositories
+
+import com.jh.controleponto.documents.Funcionario
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface FuncionarioRepository : MongoRepository<Funcionario, String> {
+
+    fun findByEmail(email: String): Funcionario
+
+    fun findByCpf(cpf: String): Funcionario
+}
